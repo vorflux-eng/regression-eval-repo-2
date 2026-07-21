8617ddf56abcfa292d27315229842a05d50fad3c
yashkumar18 <26303093+yashkumar18@users.noreply.github.com>
2026-07-21 09:50:44 +0000

    Add version endpoint

---
+++ tests/version.test.js
@@ -0,0 +1,18 @@
+const request = require("supertest");
+const app = require("../src/app");
+const { version } = require("../package.json");
+
+describe("GET /version", () => {
+  it("returns 200 with the package version", async () => {
+    const res = await request(app).get("/version");
+
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({ version });
+  });
+
+  it("returns JSON content-type", async () => {
+    const res = await request(app).get("/version");
+
+    expect(res.headers["content-type"]).toMatch(/application\/json/);
+  });
+});
