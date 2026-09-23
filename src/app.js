ff91c4727f3f1bd93946eaf77288f06af7ee733a
yashkumar18 <26303093+yashkumar18@users.noreply.github.com>
2026-09-23 10:01:58 +0000

    Add GET /version with Jest coverage

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const { version } = require("../package.json");
 
 const app = express();
 
 app.get("/healthz", (_req, res) => {
   res.status(200).json({ status: "ok" });
+});
+
+app.get("/version", (_req, res) => {
+  res.status(200).json({ version });
 });
 
 module.exports = app;
