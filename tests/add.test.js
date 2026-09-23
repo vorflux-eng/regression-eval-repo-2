b2d057b95a42c62af396783a93ec41d432ee1623
yashkumar18 <26303093+yashkumar18@users.noreply.github.com>
2026-09-23 10:14:04 +0000

    Add arithmetic helpers and Jest tests

---
+++ tests/add.test.js
@@ -0,0 +1,18 @@
+const add = require('../src/add');
+
+describe('add', () => {
+  test.each([
+    [2, 3, 5],
+    [-2, -3, -5],
+    [-2, 3, 1],
+    [0, 5, 5],
+    [5, 0, 5],
+    [0, 0, 0],
+  ])('add(%s, %s) returns %s', (a, b, expected) => {
+    expect(add(a, b)).toBe(expected);
+  });
+
+  test('adds decimal numbers', () => {
+    expect(add(0.1, 0.2)).toBeCloseTo(0.3);
+  });
+});
