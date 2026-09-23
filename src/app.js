8f4602aa5135f821fb1b95758559db1eb3f86860
yashkumar18 <26303093+yashkumar18@users.noreply.github.com>
2026-09-23 07:58:21 +0000

    Add version endpoint

--- src/app.js
+++ src/app.js
@@ -6,4 +6,8 @@ app.get("/healthz", (_req, res) => {
   res.status(200).json({ status: "ok" });
 });
 
+app.get("/version", (_req, res) => {
+  res.status(200).json({ version: require("../package.json").version });
+});
+
 module.exports = app;
