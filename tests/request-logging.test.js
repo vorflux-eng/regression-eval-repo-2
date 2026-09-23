15e004af5853e2a4cc562023921ce94bd319ee0d
yashkumar18 <26303093+yashkumar18@users.noreply.github.com>
2026-09-23 10:06:56 +0000

    Log requests except health checks

---
+++ tests/request-logging.test.js
@@ -0,0 +1,28 @@
+const request = require("supertest");
+const app = require("../src/app");
+
+describe("request logging", () => {
+  let log;
+
+  beforeEach(() => {
+    log = jest.spyOn(console, "log").mockImplementation(() => {});
+  });
+
+  afterEach(() => {
+    log.mockRestore();
+  });
+
+  it.each(["/healthz", "/healthz?probe=1"])(
+    "does not log %s",
+    async (path) => {
+      await request(app).get(path).expect(200);
+      expect(log).not.toHaveBeenCalled();
+    }
+  );
+
+  it.each(["/unknown", "/healthz-other"])("logs GET %s", async (path) => {
+    await request(app).get(path).expect(404);
+    expect(log).toHaveBeenCalledTimes(1);
+    expect(log).toHaveBeenCalledWith(`GET ${path}`);
+  });
+});
